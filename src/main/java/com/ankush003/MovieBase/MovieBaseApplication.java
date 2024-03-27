6822aa4ddf4f9228428dc04b996face56077390b
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2024-03-27 23:24:23 +0530

    Initial Commit

---
+++ src/main/java/com/ankush003/MovieBase/MovieBaseApplication.java
@@ -0,0 +1,13 @@
+package com.ankush003.MovieBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankush003/MovieBase/MovieBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankush003.MovieBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
